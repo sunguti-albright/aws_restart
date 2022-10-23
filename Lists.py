d62d632351a07550ad93de9bbd3c396d43dbff9c
Albright Sunguti <44727007+sunguti-albright@users.noreply.github.com>
2022-10-23 10:20:56 +0300

    add files via upload

---
+++ Lists.py
@@ -0,0 +1,26 @@
+"""
+Module description
+"""
+myFirstList = ["cherry", 1, "Apples", "Saudi"]
+print(myFirstList)
+print(myFirstList[2])
+myFirstList[2] = "Orange"
+print(myFirstList)
+
+
+myFinalAnswerTuple = ("apple", "banana", "pineapple")
+print(myFinalAnswerTuple)
+print(type(myFinalAnswerTuple))
+print(myFinalAnswerTuple[1])
+
+
+myFavoriteFruitDictionary = {
+  "Akua" : "apple",
+  "Saanvi" : "banana",
+  "Paulo" : "pineapple"
+}
+print(myFavoriteFruitDictionary)
+print(type(myFavoriteFruitDictionary))
+
+print(myFavoriteFruitDictionary["Paulo"])
+
